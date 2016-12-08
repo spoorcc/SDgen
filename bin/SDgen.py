f3ad7a91fe02d30776771810db37bc7e3c3b88bd
ben <ben.spoor@gmail.com>
2016-12-08 22:03:15 +0100

    Added argument parsing

--- bin/SDgen.py
+++ bin/SDgen.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
 import datetime
+import errno
 
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-from idl_parser import parser
+
+from idl_parser import parser as idl_parser
+import argparse
 
 __author__ = 'Ben'
 
@@ -38,23 +41,36 @@ header_inc_guard_close_template='''
 function_template='''
 {indent}void S_{module}{interface}_{method}('''
 
-def main(idl_str):
-  _parser = parser.IDLParser()
+
+
+def mkdir_p(path):
+    ''' Make directory if it does not exist '''
+    try:
+        os.makedirs(path)
+        logger.info('Created directory %s' % path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def process_idl_file(idl_str, outdir):
+  _parser = idl_parser.IDLParser()
   global_module = _parser.load(idl_str)
 
   for module in global_module.modules:
 
-     generate_c_module(module)
+     generate_c_module(module, outdir)
 
-def generate_c_module(module):
+def generate_c_module(module, outdir):
 
     for interface in module.interfaces:
-        generate_c_interface(module.name, interface)
+        generate_c_interface(module.name, interface, outdir)
 
 
-def generate_c_interface(module_name, interface):
+def generate_c_interface(module_name, interface, outdir):
 
-    filename = "%s_%s.h" % (module_name, interface.name)
+    filename = os.path.join(outdir, "%s_%s.h" % (module_name, interface.name))
 
     with open(filename, 'w') as header_file:
         logging.info('Creating %s' % header_file.name)
@@ -103,6 +119,18 @@ def generate_c_type(idl_type):
 
 if __name__ == '__main__':
 
-    with open(sys.argv[1],'r') as idl_file:
-      logger.info('Converting %s' % idl_file.name)
-      main(idl_file.read())
+    parser = argparse.ArgumentParser(description='Generate Service Dispatcher headers')
+    parser.add_argument('files', metavar='<idl-file>', type=str, nargs='+',
+                         help='IDL file to convert')
+    parser.add_argument('-o','--outdir',
+                    default='.',
+                    help='Output directory for generated files (default: current dir)')
+
+    args = parser.parse_args()
+
+    mkdir_p(args.outdir)
+
+    for path in args.files:
+        with open(path,'r') as idl_file:
+            logger.info('Converting %s' % idl_file.name)
+            process_idl_file(idl_file.read(), args.outdir)
